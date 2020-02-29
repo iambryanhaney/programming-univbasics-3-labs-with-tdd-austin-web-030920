c049bb06309964220b7f193318abcac6f919c111
Bryan Haney <i.am.bryan.haney@gmail.com>
2020-02-29 09:02:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,6 @@
 first_number = 89
 second_number = 11
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
